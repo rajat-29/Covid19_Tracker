9b482e62c41d436403f3d284c07721b43e87f382
Rajat Gupta <rajat295gupta@gmail.com>
2020-04-22 20:07:06 +0530

    country wise data

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var commArr;
 var country_names = document.getElementById('country_names');
-var confirmedCases = document.getElementById('confirmedCases');
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://api.covid19api.com/summary');
@@ -12,9 +11,8 @@ request.onload = function() {
 
 function fetchCountries(countryData) {
 	//console.log(countryData)
-	var len = commArr.Countries.length
-	for(var i=0;i<len;i++)
-	{
+	var len = commArr.Countries.length;
+	for(var i=0;i<len;i++) {
 		var option = document.createElement('option');
 		option.innerHTML = countryData[i].Country;
 		option.value = countryData[i].Country;
@@ -35,3 +33,14 @@ function displayResult(obj) {
 	document.getElementById('newRecoveredCases').innerHTML = obj.NewRecovered;
 	document.getElementById('newDeathCases').innerHTML = obj.NewDeaths;
 }
+
+country_names.addEventListener('change', function(event) {
+	console.log(event.target.value)
+	var len = commArr.Countries.length;
+	for(var i=0;i<len;i++) {
+		if(commArr.Countries[i].Country == event.target.value) {
+			console.log(commArr.Countries[i])
+			displayResult(commArr.Countries[i])
+		}
+	}
+})
